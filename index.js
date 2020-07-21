0c03f010e56c35d109bf98f2c42afff3ecbdabd3
Wuzup <dgr993@gmail.com>
2020-07-20 19:09:49 -0600

    added pseudo code and the start of some variables for the databases

---
+++ index.js
@@ -0,0 +1,32 @@
+//TITLES IN ALL CAPS
+const inquirer = require ("inquirer");
+const fs = require("fs");
+cons 
+
+// TITLE of the readme 
+
+//DESCRIPTION
+
+// TABLE OF CONTENTS
+    // with links for each section Description, Installation, Usage, Contributing, and Tests
+    // clicking on the links here take the user to the sections they click on.
+
+// INSTALLATION instructions
+
+// USAGE information
+
+// LICENSE from a  list of options 
+    //badge for that license added at the top of the readme
+    // notice added to this license section explaining which license the application is covered under
+
+//CONTRIBUTING contribution guidelines
+
+
+//TESTS instructions
+
+//QUESTIONS user inputs github username and email and instructions for how to reach me
+    //github username and link to github of username entered
+    //email
+
+//include one badge that is specific to the repository
+//![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://GitHub.com/Naereen/StrapDown.js/graphs/commit-activity)
